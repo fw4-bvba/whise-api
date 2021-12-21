b1e64f78e57e0f8e86d168c939d68373bbdd8c18
Sam Feyaerts <sam@fw4.be>
2021-12-21 16:46:26 +0100

    Add PossibleFloodType enum

---
+++ src/Enums/PossibleFloodType.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the fw4/whise-api library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Whise\Api\Enums;
+
+final class PossibleFloodType extends Enum
+{
+    const Effective = 1229;
+    const Potential  = 1230;
+    const None  = 1233;
+}
