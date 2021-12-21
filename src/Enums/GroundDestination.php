d126943ed8214a0b552296956a7bd5e653fa99ca
Sam Feyaerts <sam@fw4.be>
2021-12-21 16:30:31 +0100

    Add GroundDestination enum

---
+++ src/Enums/GroundDestination.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the fw4/whise-api library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Whise\Api\Enums;
+
+final class GroundDestination extends Enum
+{
+    const Agricultural = 1;
+    const EconomicActivity  = 2;
+    const Extraction = 3;
+    const Reserve = 4;
+    const Forest = 5;
+    const Industrial = 6;
+    const DayLeisure = 7;
+    const StayingLeisure = 8;
+    const MixedResidential = 9;
+    const Nature = 10;
+    const Park = 11;
+    const Residential = 12;
+    const ResidentialValuable = 13;
+    const ResidentialPark = 14;
+    const ResidentialExtension = 15;
+    const GreenBelt = 16;
+    const NatureReserve = 17;
+    const AgriculturalValuable = 18;
+    const IndustrialSME = 19;
+    const ResidentialRural = 999;
+    const Other = 1839;
+    const UrbanDevelopment = 1871;
+}
