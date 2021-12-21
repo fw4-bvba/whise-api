56d2a0f3ddbb99dd51d1f8a7e2400943aa635bb5
Sam Feyaerts <sam@fw4.be>
2021-12-21 16:46:12 +0100

    Add FloodAreaType enum

---
+++ src/Enums/FloodAreaType.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the fw4/whise-api library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Whise\Api\Enums;
+
+final class FloodAreaType extends Enum
+{
+    const FloodArea = 1231;
+    const Riverside  = 1232;
+}
