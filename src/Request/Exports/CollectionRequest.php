2c018e9b3f3b6b4e6354e303c1a4188c269ca25f
Jarno Lasseel <jarno@webatvantage.be>
2024-04-17 16:34:40 +0200

    Style fixer

--- src/Request/Exports/CollectionRequest.php
+++ src/Request/Exports/CollectionRequest.php
@@ -13,33 +13,27 @@ use Whise\Api\Request\CollectionRequest as BaseCollectionRequest;
 
 class CollectionRequest extends BaseCollectionRequest
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getBody(): ?string
-	{
-		if (is_null($this->body))
-		{
-			$this->body = [];
-		}
+    /**
+     * {@inheritdoc}
+     */
+    public function getBody(): ?string
+    {
+        if (is_null($this->body)) {
+            $this->body = [];
+        }
 
-		if (is_array($this->body))
-		{
-			if (!isset($this->body['Page']))
-			{
-				$this->body['Page'] = [];
-			}
-			$this->body['Page']['Limit'] = $this->getPageSize();
-			$this->body['Page']['Offset'] = $this->getPage();
-		}
+        if (is_array($this->body)) {
+            if (!isset($this->body['Page'])) {
+                $this->body['Page'] = [];
+            }
+            $this->body['Page']['Limit'] = $this->getPageSize();
+            $this->body['Page']['Offset'] = $this->getPage();
+        }
 
-		if (is_null($this->body) || is_string($this->body))
-		{
-			return $this->body;
-		}
-		else
-		{
-			return json_encode($this->encode($this->body));
-		}
-	}
+        if (is_null($this->body) || is_string($this->body)) {
+            return $this->body;
+        } else {
+            return json_encode($this->encode($this->body));
+        }
+    }
 }
